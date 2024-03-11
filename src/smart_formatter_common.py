b421120b0c72bf80fa90d1c02a0b4d1c7c526095
Zupanico <nicolas.garant@gmail.com>
2024-03-11 15:47:49 -0400

    Revert "chore:tabs"
    This reverts commit f1848ab43d3fe9bb6fcecfcec15c4cc10d05464c.

--- src/smart_formatter_common.py
+++ src/smart_formatter_common.py
@@ -3,12 +3,12 @@
 
 
 """
-	Utilitaire pour imprimer les commentaires d'aide sur plusieurs lignes (dans argparse)
+    Utilitaire pour imprimer les commentaires d'aide sur plusieurs lignes (dans argparse)
 
-	Note :
-		- La classe SmartFormatter permet d'imprimer correctement la chaîne de caractère "help" lorsqu'elle débute par "R|"
+    Note :
+        - La classe SmartFormatter permet d'imprimer correctement la chaîne de caractère "help" lorsqu'elle débute par "R|"
 
-	Copyright 2024, Frédéric Mailhot et Université de Sherbrooke
+    Copyright 2024, Frédéric Mailhot et Université de Sherbrooke
 
 """
 
@@ -16,14 +16,14 @@ import argparse
 
 
 class SmartFormatter(argparse.HelpFormatter):
-	"""Classe destinée à formatter l'affichage des messages d'aide de argparse
+    """Classe destinée à formatter l'affichage des messages d'aide de argparse
 
-		- Méthode incluse : _split_lines, qui remplace ce qui est fourni par défaut par argparse
+        - Méthode incluse : _split_lines, qui remplace ce qui est fourni par défaut par argparse
 
 """
 
-	def _split_lines(self, text: str, width: int):
-		if text.startswith('R|'):
-			return text[2:].splitlines()
-			# this is the RawTextHelpFormatter._split_lines
-		return argparse.HelpFormatter._split_lines(self, text, width)
+    def _split_lines(self, text: str, width: int):
+        if text.startswith('R|'):
+            return text[2:].splitlines()
+            # this is the RawTextHelpFormatter._split_lines
+        return argparse.HelpFormatter._split_lines(self, text, width)
